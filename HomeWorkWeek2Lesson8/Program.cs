7faba7dcb639db5eae1bec8d108e2796f186df9a
skochansky <stachu.kochanski@gmail.com>
2020-08-09 15:00:35 +0200

    #Done exercise 1/10

---
+++ HomeWorkWeek2Lesson8/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace HomeWorkWeek2Lesson8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExerciseOne();
+        }
+        
+        public static void ExerciseOne()
+        {
+            int count = 0; 
+            int prime = 0; 
+            
+            for (int i = 1; i <= 1000; i++)
+            {
+                for (int j = 2; j < i; j++) 
+                {
+                    if (i % j != 0)
+                    {
+                        count += 1;
+                    }
+                }
+                if (count == (i - 2))
+                {
+                    prime++;
+                }
+            
+                count = 0;
+            }
+            Console.WriteLine(prime);
+        }
+    }
+       
+}
